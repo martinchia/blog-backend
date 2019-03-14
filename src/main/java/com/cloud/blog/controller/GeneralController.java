a1316438505504b70e24177439f7f39cf6f82b72
martinchia <martinchia93@outlook.com>
2019-03-13 21:34:46 -0400

    controller modification

--- src/main/java/com/cloud/blog/controller/GeneralController.java
+++ src/main/java/com/cloud/blog/controller/GeneralController.java
@@ -3,6 +3,7 @@ package com.cloud.blog.controller;
 import com.cloud.blog.error.BusinessException;
 import com.cloud.blog.error.EmBusinessError;
 import com.cloud.blog.response.CommonReturnType;
+import javafx.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class GeneralController {
     public static final String CONTENT_TYPE_FORMED = "application/json";
@@ -20,6 +23,7 @@ public class GeneralController {
     @ResponseBody
     public Object handlerException(HttpServletRequest request, Exception ex) {
         Map<String, Object> response = new HashMap<>();
+        Queue<Pair<String, Integer>> queue = new PriorityQueue<>((x, y) -> y.getValue() - x.getValue());
         if(ex instanceof BusinessException) {
             BusinessException businessException = (BusinessException) ex;
             response.put("errorCode", businessException.getErrorCode());
